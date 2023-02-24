eba51bec7401ab7a00c098d29f63b37a9b13b4d3
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-02-24 12:46:06 +0100

    add 2 node clusters

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
@@ -7,15 +8,23 @@ function delay (duration) {
   while(Date.now() - startTime < duration){
     // block the event loop
   }
-}
+};
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  res.send(`Performance example ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding!');
-})
+  res.send(`Ding ding ding! ${process.pid}`);
+});
 
-app.listen(3000);
+console.log('server.js is running...');
+if(cluster.isMaster) {
+  console.log('Master has starter');
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('worker process started');
+  app.listen(3000);
+}
